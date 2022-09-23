f150cf2411d997c003dd3c775f8454252a65dcec
chLi <565353780@qq.com>
2022-09-24 02:35:36 +0800

    add generate image algo

--- auto_cad_recon/Method/image.py
+++ auto_cad_recon/Method/image.py
@@ -6,3 +6,10 @@ import numpy as np
 
 def getObjectImage(point_idx_list, image):
     object_image = np.zeros(image.shape)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            pixel_idx = i * image.shape[0] + j
+            if pixel_idx not in point_idx_list:
+                continue
+            object_image[i][j] = image[i][j]
+    return object_image
