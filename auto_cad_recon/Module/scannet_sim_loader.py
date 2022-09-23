305f7070d1b39cf55a4cb4b764d61b0bfad0aed4
chLi <565353780@qq.com>
2022-09-23 16:48:14 +0800

    finish trans colors and positions

--- auto_cad_recon/Module/scannet_sim_loader.py
+++ auto_cad_recon/Module/scannet_sim_loader.py
@@ -14,14 +14,15 @@ import quaternion
 import numpy as np
 import open3d as o3d
 from getch import getch
-
-from auto_cad_recon.Method.depth import get_point_cloud_from_z
+from multiprocessing import Process
 
 
 class ScanNetSimLoader(object):
 
     def __init__(self):
         self.sim_manager = SimManager()
+
+        self.pcd_list = []
         return
 
     def reset(self):
@@ -47,11 +48,9 @@ class ScanNetSimLoader(object):
         W = 480
         H = 360
 
-        K = np.array([
-            [1 / np.tan(hfov / 2.), 0., 0., 0.],
-            [0., 1 / np.tan(hfov / 2.), 0., 0.],
-            [0., 0.,  1, 0],
-            [0., 0., 0, 1]])
+        K = np.array([[1 / np.tan(hfov / 2.), 0., 0., 0.],
+                      [0., 1 / np.tan(hfov / 2.), 0., 0.], [0., 0., 1, 0],
+                      [0., 0., 0, 1]])
 
         agent_state = self.sim_manager.sim_loader.getAgentState()
 
@@ -59,29 +58,41 @@ class ScanNetSimLoader(object):
         translation = agent_state.sensor_states['depth_sensor'].position
         rotation = quaternion.as_rotation_matrix(rotation)
         T_world_camera = np.eye(4)
-        T_world_camera[0:3,0:3] = rotation
-        T_world_camera[0:3,3] = translation
+        T_world_camera[0:3, 0:3] = rotation
+        T_world_camera[0:3, 3] = translation
 
-        T_camera_world = np.linalg.inv(T_world_camera)
-
-        xs, ys = np.meshgrid(np.linspace(-1,1,W), np.linspace(1,-1,H))
-        depth = depth_obs.reshape(1,W,H)
-        xs = xs.reshape(1,W,H)
-        ys = ys.reshape(1,W,H)
-        xys = np.vstack((xs * depth , ys * depth, -depth, np.ones(depth.shape)))
+        xs, ys = np.meshgrid(np.linspace(-1, 1, W), np.linspace(1, -1, H))
+        depth = depth_obs.reshape(1, W, H)
+        xs = xs.reshape(1, W, H)
+        ys = ys.reshape(1, W, H)
+        xys = np.vstack((xs * depth, ys * depth, -depth, np.ones(depth.shape)))
         xys = xys.reshape(4, -1)
         xy_c0 = np.matmul(np.linalg.inv(K), xys)
-        point_array = np.matmul(T_camera_world, xy_c0)
+        point_array = np.matmul(T_world_camera, xy_c0)
 
         point_list = []
         for i in range(W * H):
             point_list.append(point_array[:3, i])
 
         points = np.array(point_list)
+        color_list = []
+        for i in range(rgb_obs.shape[0]):
+            for j in range(rgb_obs.shape[1]):
+                color_list.append(rgb_obs[i][j])
+        colors = np.array(color_list)
 
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points)
-        o3d.visualization.draw_geometries([pcd])
+        pcd.colors = o3d.utility.Vector3dVector(colors / 255.0)
+
+        self.pcd_list.append(pcd)
+
+        process = Process(target=self.render)
+        process.start()
+        return True
+
+    def render(self):
+        o3d.visualization.draw_geometries(self.pcd_list)
         return True
 
     def startKeyBoardControlRender(self, wait_val):
