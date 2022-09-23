aaa6e19a1890976f69c663bf6e3f16252898aedc
chLi <565353780@qq.com>
2022-09-23 21:03:03 +0800

    now will generate a dict data

--- auto_cad_recon/Method/object.py
+++ auto_cad_recon/Method/object.py
@@ -9,10 +9,8 @@ from auto_cad_recon.Data.bbox import BBox
 from auto_cad_recon.Method.bbox import getPointInBBoxIdxListDict
 
 
-def getInViewObjectFileNameList(bbox_json_file_path, color_point_list):
+def getInViewObjectFileNameDict(bbox_json_file_path, color_point_list):
     assert os.path.exists(bbox_json_file_path)
-
-    in_view_object_file_list = []
 
     with open(bbox_json_file_path, "r") as f:
         data = f.read()
@@ -23,6 +21,16 @@ def getInViewObjectFileNameList(bbox_json_file_path, color_point_list):
         bbox_dict[object_file_name] = BBox.fromList(bbox_list)
 
     point_in_bbox_idx_list_dict = getPointInBBoxIdxListDict(
-        color_point_list, list(bbox_dict.values()))
+        color_point_list, bbox_dict.values())
 
-    return bbox_dict.values()
+    object_file_name_list = list(bbox_dict.keys())
+
+    in_view_object_file_name_dict = {}
+    for point_idx, point_in_bbox_idx_list in point_in_bbox_idx_list_dict.items(
+    ):
+        in_view_object_file_name_dict[point_idx] = [
+            object_file_name_list[point_in_bbox_idx]
+            for point_in_bbox_idx in point_in_bbox_idx_list
+        ]
+
+    return point_in_bbox_idx_list_dict
