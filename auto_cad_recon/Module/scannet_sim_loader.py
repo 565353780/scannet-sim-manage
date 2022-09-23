ff31ff59efda1142c195ab15de79bb52c09638fe
chLi <565353780@qq.com>
2022-09-23 19:25:48 +0800

    add object bbox visual algo, axis not matched

--- auto_cad_recon/Module/scannet_sim_loader.py
+++ auto_cad_recon/Module/scannet_sim_loader.py
@@ -9,6 +9,9 @@ from habitat_sim_manage.Data.rad import Rad
 from habitat_sim_manage.Data.pose import Pose
 from habitat_sim_manage.Module.sim_manager import SimManager
 
+sys.path.append("../image-to-cad")
+from image_to_cad.Method.bboxes import getOpen3DBBoxFromBBox
+
 import os
 import numpy as np
 import open3d as o3d
@@ -16,40 +19,54 @@ from getch import getch
 from multiprocessing import Process
 
 from auto_cad_recon.Method.depth import getColorPointList
+from auto_cad_recon.Method.object import getInViewObjectFileNameList
 
 
 class ScanNetSimLoader(object):
 
     def __init__(self):
         self.sim_manager = SimManager()
+        self.scene_name = None
         return
 
     def reset(self):
         self.sim_manager.reset()
+        self.scene_name = None
         return True
 
     def loadScene(self, glb_file_path):
         assert os.path.exists(glb_file_path)
         self.sim_manager.loadSettings(glb_file_path)
+        self.scene_name = glb_file_path.split("/")[-2]
         return True
 
     def setControlMode(self, control_mode):
         return self.sim_manager.setControlMode(control_mode)
 
     def getObjectInView(self):
+        bbox_json_file_path = "/home/chli/chLi/ScanNet/bboxes/scene0474_02/object_bbox.json"
+
         observations = self.sim_manager.sim_loader.observations
         agent_state = self.sim_manager.sim_loader.getAgentState()
 
         color_point_list = getColorPointList(observations, agent_state)
 
-        points_colors = np.array([color_point.toList() for color_point in color_point_list])
+        in_view_object_file_name_list = getInViewObjectFileNameList(
+            bbox_json_file_path, color_point_list)
+
+        o3d_bbox_list = []
+        for bbox in in_view_object_file_name_list:
+            o3d_bbox_list.append(getOpen3DBBoxFromBBox(bbox))
+
+        points_colors = np.array(
+            [color_point.toList() for color_point in color_point_list])
 
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(points_colors[:, :3])
         pcd.colors = o3d.utility.Vector3dVector(points_colors[:, 3:] / 255.0)
 
         process = Process(target=o3d.visualization.draw_geometries,
-                          args=([pcd], ))
+                          args=([pcd] + o3d_bbox_list, ))
         process.start()
         return True
 
