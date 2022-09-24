7d3d1a93ad81c8402a80b4bb7ed2f89f339dbdaf
chLi <565353780@qq.com>
2022-09-24 16:08:47 +0800

    finish get label image

--- auto_cad_recon/Data/point_image.py
+++ auto_cad_recon/Data/point_image.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 from auto_cad_recon.Method.depth import getPointArray
 
 
@@ -9,7 +11,7 @@ class PointImage(object):
     def __init__(self, observations=None, agent_state=None):
         self.image = None
         self.point_array = None
-        self.label_list_list = []
+        self.label_dict_list = []
 
         if observations is not None and agent_state is not None:
             self.loadObservations(observations, agent_state)
@@ -18,7 +20,7 @@ class PointImage(object):
     def loadObservations(self, observations, agent_state):
         self.image = observations["color_sensor"][..., :3]
         self.point_array = getPointArray(observations, agent_state)
-        self.label_list_list = [[] for _ in self.point_array]
+        self.label_dict_list = [{} for _ in self.point_array]
         return True
 
     def getArrayIdx(self, pixel_idx):
@@ -27,10 +29,27 @@ class PointImage(object):
 
     def getPixelIdx(self, array_idx):
         assert self.image is not None
-        return [int(array_idx / self.image.shape[0]), array_idx % self.image.shape[0]]
+        return [
+            int(array_idx / self.image.shape[0]),
+            array_idx % self.image.shape[0]
+        ]
 
-    def addLabel(self, array_idx, label):
+    def addLabel(self, array_idx, label, value=True):
         assert self.image is not None
         assert array_idx < len(self.point_array)
-        self.label_list_list[array_idx].append(label)
+        self.label_dict_list[array_idx][label] = value
         return True
+
+    def getLabelImage(self, label, value=True):
+        label_image = np.zeros(self.image.shape, dtype=np.uint8)
+
+        for i, label_dict in enumerate(self.label_dict_list):
+            label_dict = self.label_dict_list[i]
+            if label not in label_dict.keys():
+                continue
+            if label_dict[label] != value:
+                continue
+            pixel_idx = self.getPixelIdx(i)
+            label_image[pixel_idx[0]][pixel_idx[1]] = self.image[pixel_idx[0]][
+                pixel_idx[1]]
+        return label_image
