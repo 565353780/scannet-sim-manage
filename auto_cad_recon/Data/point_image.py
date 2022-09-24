620ca4d3e6ca14a7bf9c839c6e442c5c70e6039d
chLi <565353780@qq.com>
2022-09-24 21:00:29 +0800

    add empty check, set white as default background

--- auto_cad_recon/Data/point_image.py
+++ auto_cad_recon/Data/point_image.py
@@ -11,7 +11,9 @@ class PointImage(object):
 
     def __init__(self, observations=None, agent_state=None):
         self.image = None
+        self.depth = None
         self.point_array = None
+        self.camera_point = None
         self.label_dict_list = []
 
         if observations is not None and agent_state is not None:
@@ -20,8 +22,31 @@ class PointImage(object):
 
     def loadObservations(self, observations, agent_state):
         self.image = observations["color_sensor"][..., :3][..., ::-1]
-        self.point_array = getPointArray(observations, agent_state)
+        self.depth = observations["depth_sensor"]
+        self.point_array, self.camera_point = getPointArray(
+            observations, agent_state)
         self.label_dict_list = [{} for _ in self.point_array]
+
+        match_x_idx = np.where(self.point_array[:,
+                                                0] == self.camera_point[0])[0]
+        match_x_point_array = self.point_array[match_x_idx]
+
+        x_match_y_idx = np.where(
+            match_x_point_array[:, 1] == self.camera_point[1])[0]
+        match_xy_idx = match_x_idx[x_match_y_idx]
+        match_xy_point_array = self.point_array[match_xy_idx]
+
+        xy_match_z_idx = np.where(
+            match_xy_point_array[:, 2] == self.camera_point[2])[0]
+        match_xyz_idx = match_xy_idx[xy_match_z_idx]
+
+        self.point_array[match_xyz_idx] = [
+            float("inf"), float("inf"),
+            float("inf")
+        ]
+
+        for empty_idx in match_xyz_idx:
+            self.addLabel(empty_idx, "empty")
         return True
 
     def getArrayIdx(self, pixel_idx):
@@ -38,11 +63,12 @@ class PointImage(object):
     def addLabel(self, array_idx, label, value=True):
         assert self.image is not None
         assert array_idx < len(self.point_array)
+        assert "empty" not in self.label_dict_list[array_idx].keys()
         self.label_dict_list[array_idx][label] = value
         return True
 
     def getLabelImage(self, label, value=True):
-        label_image = np.zeros(self.image.shape, dtype=np.uint8)
+        label_image = np.ones(self.image.shape, dtype=np.uint8) * 255
 
         for i, label_dict in enumerate(self.label_dict_list):
             label_dict = self.label_dict_list[i]
@@ -56,7 +82,7 @@ class PointImage(object):
         return label_image
 
     def getAllLabelImage(self):
-        all_label_image = np.zeros(self.image.shape, dtype=np.uint8)
+        all_label_image = np.ones(self.image.shape, dtype=np.uint8) * 255
 
         background_color = [0, 0, 0]
 
@@ -64,27 +90,42 @@ class PointImage(object):
         value_list = []
         color_list = []
         for label_dict in self.label_dict_list:
+            if "empty" in label_dict.keys():
+                continue
             for label, value in label_dict.items():
-                if label not in label_list:
-                    label_list.append(label)
-                    value_list.append(value)
-                    color_list.append(
-                        [randint(0, 255),
-                         randint(0, 255),
-                         randint(0, 255)])
+                if label in label_list or \
+                        label == "background" or \
+                        value not in ["object", True]:
+                    continue
+
+                label_list.append(label)
+                value_list.append(value)
+                color_list.append(
+                    [randint(0, 255),
+                     randint(0, 255),
+                     randint(0, 255)])
 
         for i, label_dict in enumerate(self.label_dict_list):
             label_dict = self.label_dict_list[i]
-            for j, label in enumerate(label_list):
+
+            if "empty" in label_dict.keys():
+                continue
+
+            if "background" in label_dict.keys():
+                pixel_idx = self.getPixelIdx(i)
+                all_label_image[pixel_idx[0], pixel_idx[1], :] = \
+                    background_color
+                continue
+
+            for j, [label, value] in enumerate(zip(label_list, value_list)):
                 if label not in label_dict.keys():
                     continue
-                if label_dict[label] not in ["object", True]:
+                if label_dict[label] != value:
                     continue
-                color = color_list[j]
-                if label == "background":
-                    color = background_color
-
+                if value not in ["object", True]:
+                    continue
                 pixel_idx = self.getPixelIdx(i)
-                all_label_image[pixel_idx[0], pixel_idx[1], :] = color
+                all_label_image[pixel_idx[0], pixel_idx[1], :] = \
+                    color_list[j]
                 break
         return all_label_image
