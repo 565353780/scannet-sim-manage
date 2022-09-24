3018afd7264cf7e62d3a01cbf4c3cdf1779ec8be
chLi <565353780@qq.com>
2022-09-24 18:17:22 +0800

    fix idx trans bug

--- auto_cad_recon/Data/point_image.py
+++ auto_cad_recon/Data/point_image.py
@@ -18,20 +18,20 @@ class PointImage(object):
         return
 
     def loadObservations(self, observations, agent_state):
-        self.image = observations["color_sensor"][..., :3]
+        self.image = observations["color_sensor"][..., :3][..., ::-1]
         self.point_array = getPointArray(observations, agent_state)
         self.label_dict_list = [{} for _ in self.point_array]
         return True
 
     def getArrayIdx(self, pixel_idx):
         assert self.image is not None
-        return pixel_idx[0] * self.image.shape[0] + pixel_idx[1]
+        return pixel_idx[0] * self.image.shape[1] + pixel_idx[1]
 
     def getPixelIdx(self, array_idx):
         assert self.image is not None
         return [
-            int(array_idx / self.image.shape[0]),
-            array_idx % self.image.shape[0]
+            int(array_idx / self.image.shape[1]),
+            array_idx % self.image.shape[1]
         ]
 
     def addLabel(self, array_idx, label, value=True):
@@ -50,6 +50,6 @@ class PointImage(object):
             if label_dict[label] != value:
                 continue
             pixel_idx = self.getPixelIdx(i)
-            label_image[pixel_idx[0]][pixel_idx[1]] = self.image[pixel_idx[0]][
-                pixel_idx[1]]
+            label_image[pixel_idx[0], pixel_idx[1], :] = \
+                self.image[pixel_idx[0],  pixel_idx[1], :]
         return label_image
