1bb68de5ad1053a1bdd5da888252ccd1ad8e1042
chLi <565353780@qq.com>
2022-09-25 15:49:22 +0800

    split demo into Demo folder, update import path

---
+++ auto_cad_recon/Demo/scannet_sim_loader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+sys.path.append("../habitat-sim-manage")
+from habitat_sim_manage.Data.point import Point
+from habitat_sim_manage.Data.rad import Rad
+from habitat_sim_manage.Data.pose import Pose
+
+from auto_cad_recon.Module.scannet_sim_loader import ScanNetSimLoader
+
+
+def demo():
+    glb_file_path = \
+        "/home/chli/chLi/ScanNet/scans/scene0474_02/scene0474_02_vh_clean.glb"
+    object_folder_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/"
+    bbox_json_file_path = "/home/chli/chLi/ScanNet/bboxes/scene0474_02/object_bbox.json"
+    control_mode = "pose"
+    wait_val = 1
+    print_progres = True
+
+    scannet_sim_loader = ScanNetSimLoader()
+    scannet_sim_loader.loadScene(glb_file_path, object_folder_path,
+                                 bbox_json_file_path, print_progres)
+    scannet_sim_loader.setControlMode(control_mode)
+
+    scannet_sim_loader.sim_manager.pose_controller.pose = Pose(
+        Point(3.7, 1.0, -2.6), Rad(0.2, 0.0))
+    scannet_sim_loader.sim_manager.sim_loader.setAgentState(
+        scannet_sim_loader.sim_manager.pose_controller.getAgentState())
+
+    scannet_sim_loader.startKeyBoardControlRender(wait_val)
+    return True
--- auto_cad_recon/Module/scannet_sim_loader.py
+++ auto_cad_recon/Module/scannet_sim_loader.py
@@ -4,9 +4,6 @@
 import sys
 
 sys.path.append("../habitat-sim-manage")
-from habitat_sim_manage.Data.point import Point
-from habitat_sim_manage.Data.rad import Rad
-from habitat_sim_manage.Data.pose import Pose
 from habitat_sim_manage.Module.sim_manager import SimManager
 
 import os
@@ -69,6 +66,8 @@ class ScanNetSimLoader(object):
         assert saveLabelImages(point_image, "./test/")
 
         assert renderPointImage(point_image)
+        #  assert renderBBox(self.scene_object_dist_calculator.bbox_dict)
+        #  assert renderAll(point_image, self.scene_object_dist_calculator.bbox_dict)
         return True
 
     def startKeyBoardControlRender(self, wait_val):
@@ -93,26 +92,3 @@ class ScanNetSimLoader(object):
                 break
         self.sim_manager.cv_renderer.close()
         return True
-
-
-def demo():
-    glb_file_path = \
-        "/home/chli/chLi/ScanNet/scans/scene0474_02/scene0474_02_vh_clean.glb"
-    object_folder_path = "/home/chli/chLi/ScanNet/objects/scene0474_02/"
-    bbox_json_file_path = "/home/chli/chLi/ScanNet/bboxes/scene0474_02/object_bbox.json"
-    control_mode = "pose"
-    wait_val = 1
-    print_progres = True
-
-    scannet_sim_loader = ScanNetSimLoader()
-    scannet_sim_loader.loadScene(glb_file_path, object_folder_path,
-                                 bbox_json_file_path, print_progres)
-    scannet_sim_loader.setControlMode(control_mode)
-
-    scannet_sim_loader.sim_manager.pose_controller.pose = Pose(
-        Point(3.7, 1.0, -2.6), Rad(0.2, 0.0))
-    scannet_sim_loader.sim_manager.sim_loader.setAgentState(
-        scannet_sim_loader.sim_manager.pose_controller.getAgentState())
-
-    scannet_sim_loader.startKeyBoardControlRender(wait_val)
-    return True
