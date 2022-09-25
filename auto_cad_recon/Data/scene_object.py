7d86ae50040114936245e94f0fcc8a5577d2ae22
chLi <565353780@qq.com>
2022-09-25 18:12:26 +0800

    now will auto manage all frame object

--- auto_cad_recon/Data/scene_object.py
+++ auto_cad_recon/Data/scene_object.py
@@ -8,8 +8,24 @@ class SceneObject(object):
 
     def __init__(self, object_label):
         self.object_label = object_label
-        self.frame_object_list = []
+        self.frame_object_dict = {}
+        return
 
-    def addFrameObject(self, color_point_set, image):
-        self.frame_object_list.append(FrameObject(color_point_set, image))
+    def reset(self):
+        self.object_label = "Unknown"
+        del self.frame_object_dict
+        self.frame_object_dict = {}
         return True
+
+    def addFrameObject(self, frame_idx, point_image, label, value=True):
+        self.frame_object_dict[str(frame_idx)] = FrameObject(
+            point_image, label, value)
+        return True
+
+    def getFrameObject(self, frame_idx):
+        if str(frame_idx) not in self.frame_object_dict.keys():
+            print("[WARN][SceneObject::getFrameObject]")
+            print("\t this frame_idx [" + frame_idx + "] not exist!")
+            return None
+
+        return self.frame_object_dict[str(frame_idx)]
