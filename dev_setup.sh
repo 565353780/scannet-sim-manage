7fc7efca07fcbbebe31f0937258529e2e432a66f
chLi <565353780@qq.com>
2022-09-25 19:44:54 +0800

    add install command

---
+++ dev_setup.sh
@@ -0,0 +1,12 @@
+cd ..
+
+git clone git@github.com:565353780/mesh-manage.git
+cd mesh-manage
+./dev_setup.sh
+cd ..
+
+git clone git@github.com:565353780/habitat-sim-manage.git
+cd habitat-sim-manage
+./dev_setup.sh
+cd ..
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+cd ..
+
+git clone https://github.com/565353780/mesh-manage.git
+cd mesh-manage
+./setup.sh
+cd ..
+
+
+git clone https://github.com/565353780/habitat-sim-manage.git
+cd habitat-sim-manage
+./setup.sh
+cd ..
+
