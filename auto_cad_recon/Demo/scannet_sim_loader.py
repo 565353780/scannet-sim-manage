03e82235265c8a330b5ff2d81f44d4529f92a22f
chLi <565353780@qq.com>
2022-09-25 18:50:55 +0800

    add save scene objects func, now can call it in demo script

--- auto_cad_recon/Demo/scannet_sim_loader.py
+++ auto_cad_recon/Demo/scannet_sim_loader.py
@@ -18,11 +18,13 @@ def demo():
     bbox_json_file_path = "/home/chli/chLi/ScanNet/bboxes/scene0474_02/object_bbox.json"
     control_mode = "pose"
     wait_val = 1
-    print_progres = True
+    print_progress = True
+
+    scene_objects_save_folder_path = "./output/scene_objects/scene0474_02/"
 
     scannet_sim_loader = ScanNetSimLoader()
     scannet_sim_loader.loadScene(glb_file_path, object_folder_path,
-                                 bbox_json_file_path, print_progres)
+                                 bbox_json_file_path, print_progress)
     scannet_sim_loader.setControlMode(control_mode)
 
     scannet_sim_loader.sim_manager.pose_controller.pose = Pose(
@@ -31,4 +33,6 @@ def demo():
         scannet_sim_loader.sim_manager.pose_controller.getAgentState())
 
     scannet_sim_loader.startKeyBoardControlRender(wait_val)
+
+    scannet_sim_loader.saveAllSceneObjects(scene_objects_save_folder_path)
     return True
