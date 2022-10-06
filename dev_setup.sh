cb93e75aeb6957c74979edc7ff67f568f2acfd0c
chLi <565353780@qq.com>
2022-10-06 17:01:44 +0800

    simplify setup

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,12 +1,11 @@
 cd ..
-
 git clone git@github.com:565353780/mesh-manage.git
+git clone git@github.com:565353780/habitat-sim-manage.git
+
 cd mesh-manage
 ./dev_setup.sh
-cd ..
 
-git clone git@github.com:565353780/habitat-sim-manage.git
-cd habitat-sim-manage
+cd ../habitat-sim-manage
 ./dev_setup.sh
 cd ..
 
--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 cd ..
-
 git clone https://github.com/565353780/mesh-manage.git
+git clone https://github.com/565353780/habitat-sim-manage.git
+
 cd mesh-manage
 ./setup.sh
-cd ..
 
-
-git clone https://github.com/565353780/habitat-sim-manage.git
-cd habitat-sim-manage
+cd ../habitat-sim-manage
 ./setup.sh
-cd ..
 
