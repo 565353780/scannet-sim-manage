bb5e04baaa7baf6770b4d07cd78dd8af4619f4f7
chLi <565353780@qq.com>
2022-10-17 17:22:37 +0800

    add open3d support, speed up a lot

--- scannet_sim_manage/Module/scene_object_dist_calculator.py
+++ scannet_sim_manage/Module/scene_object_dist_calculator.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import json
-from tqdm import tqdm
+import os
 
+import numpy as np
+import open3d as o3d
 from mesh_manage.Module.channel_mesh import ChannelMesh
+from tqdm import tqdm
 
 from scannet_sim_manage.Data.bbox import BBox
 from scannet_sim_manage.Data.point import Point
+
+load_method_list = ['channelmesh', 'open3d']
+load_method = 'open3d'
 
 
 class SceneObjectDistCalculator(object):
@@ -23,6 +28,8 @@ class SceneObjectDistCalculator(object):
         self.channel_mesh_dict = {}
         self.bbox_dict = {}
 
+        self.open3d_mesh_dict = {}
+
         if object_folder_path is not None:
             self.loadSceneObject(object_folder_path, print_progress)
         if bbox_json_file_path is not None:
@@ -33,17 +40,22 @@ class SceneObjectDistCalculator(object):
         del self.channel_mesh_dict
         self.channel_mesh_dict = {}
         self.bbox_dict = {}
+
+        del self.open3d_mesh_dict
+        self.open3d_mesh_dict = {}
         return True
 
-    def loadSceneObject(self,
-                        object_folder_path,
-                        print_progress=False,
-                        valid_object_file_name_list=None):
+    def loadSceneObjectByChannelMesh(self,
+                                     object_folder_path,
+                                     print_progress=False,
+                                     valid_object_file_name_list=None):
         assert os.path.exists(object_folder_path)
         object_file_name_list = os.listdir(object_folder_path)
         for_data = object_file_name_list
         if print_progress:
-            print("[INFO][SceneObjectDistCalculator::loadSceneObject]")
+            print(
+                "[INFO][SceneObjectDistCalculator::loadSceneObjectByChannelMesh]"
+            )
             print("\t start load scene objects...")
             for_data = tqdm(for_data)
         for object_file_name in for_data:
@@ -56,7 +68,46 @@ class SceneObjectDistCalculator(object):
                 object_file_path)
         return True
 
-    def loadObjectBBox(self, bbox_json_file_path, valid_object_file_name_list=None):
+    def loadSceneObjectByOpen3D(self,
+                                object_folder_path,
+                                print_progress=False,
+                                valid_object_file_name_list=None):
+        assert os.path.exists(object_folder_path)
+        object_file_name_list = os.listdir(object_folder_path)
+        for_data = object_file_name_list
+        if print_progress:
+            print("[INFO][SceneObjectDistCalculator::loadSceneObjectByOpen3D]")
+            print("\t start load scene objects...")
+            for_data = tqdm(for_data)
+        for object_file_name in for_data:
+            if valid_object_file_name_list is not None:
+                if object_file_name not in valid_object_file_name_list:
+                    continue
+
+            object_file_path = object_folder_path + object_file_name
+            self.open3d_mesh_dict[object_file_name] = o3d.io.read_point_cloud(
+                object_file_path)
+        return True
+
+    def loadSceneObject(self,
+                        object_folder_path,
+                        print_progress=False,
+                        valid_object_file_name_list=None):
+        assert load_method in load_method_list
+
+        if load_method == 'channelmesh':
+            return self.loadSceneObjectByChannelMesh(
+                object_folder_path, print_progress,
+                valid_object_file_name_list)
+
+        if load_method == 'open3d':
+            return self.loadSceneObjectByOpen3D(object_folder_path,
+                                                print_progress,
+                                                valid_object_file_name_list)
+
+    def loadObjectBBox(self,
+                       bbox_json_file_path,
+                       valid_object_file_name_list=None):
         assert os.path.exists(bbox_json_file_path)
 
         with open(bbox_json_file_path, "r") as f:
@@ -72,10 +123,14 @@ class SceneObjectDistCalculator(object):
             self.bbox_dict[object_file_name] = BBox.fromList(bbox_list)
         return True
 
-    def generateBBoxLabel(self, point_image, print_progress=False):
+    def generateBBoxLabelByChannelMesh(self,
+                                       point_image,
+                                       print_progress=False):
         for_data = point_image.point_array
         if print_progress:
-            print("[INFO][SceneObjectDistCalculator::generateBBoxLabel]")
+            print(
+                "[INFO][SceneObjectDistCalculator::generateBBoxLabelByChannelMesh]"
+            )
             print("\t start add bbox label...")
             for_data = tqdm(for_data)
         for i, [x, y, z] in enumerate(for_data):
@@ -85,10 +140,17 @@ class SceneObjectDistCalculator(object):
                     point_image.addLabel(i, object_file_name, "bbox")
         return point_image
 
-    def generateObjectLabel(self, point_image, print_progress=False):
+    def generateObjectLabelByChannelMesh(self,
+                                         point_image,
+                                         print_progress=False):
+        point_image = self.generateBBoxLabelByChannelMesh(
+            point_image, print_progress)
+
         for_data = point_image.point_array
         if print_progress:
-            print("[INFO][SceneObjectDistCalculator::generateObjectLabel]")
+            print(
+                "[INFO][SceneObjectDistCalculator::generateObjectLabelByChannelMesh]"
+            )
             print("\t start add object label...")
             for_data = tqdm(for_data)
         for i, [x, y, z] in enumerate(for_data):
@@ -99,9 +161,46 @@ class SceneObjectDistCalculator(object):
 
                 dist = self.channel_mesh_dict[object_file_name].getNearestDist(
                     x, y, z)
+
                 if dist <= self.dist_error_max:
                     point_image.addLabel(i, object_file_name, "object")
         return point_image
+
+    def generateObjectLabelByOpen3D(self, point_image, print_progress=False):
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(point_image.point_array)
+
+        for_data = self.bbox_dict.keys()
+        if print_progress:
+            print(
+                "[INFO][SceneObjectDistCalculator::generateObjectLabelByOpen3D]"
+            )
+            print("\t start add object label...")
+            for_data = tqdm(for_data)
+        for object_file_name in for_data:
+            dist_array = np.array(
+                pcd.compute_point_cloud_distance(
+                    self.open3d_mesh_dict[object_file_name]))
+            object_point_idx_array = np.where(
+                dist_array <= self.dist_error_max)[0]
+            for object_point_idx in object_point_idx_array:
+                if "empty" in point_image.label_dict_list[
+                        object_point_idx].keys():
+                    continue
+                point_image.addLabel(object_point_idx, object_file_name,
+                                     "object")
+        return point_image
+
+    def generateObjectLabel(self, point_image, print_progress=False):
+        assert load_method in load_method_list
+
+        if load_method == 'channelmesh':
+            return self.generateObjectLabelByChannelMesh(
+                point_image, print_progress)
+
+        if load_method == 'open3d':
+            return self.generateObjectLabelByOpen3D(point_image,
+                                                    print_progress)
 
     def generateBackgroundLabel(self, point_image):
         for i, label_dict in enumerate(point_image.label_dict_list):
@@ -117,8 +216,6 @@ class SceneObjectDistCalculator(object):
         return point_image
 
     def getLabeledPointImage(self, point_image, print_progress=False):
-        point_image = self.generateBBoxLabel(point_image, print_progress)
-
         point_image = self.generateObjectLabel(point_image, print_progress)
 
         point_image = self.generateBackgroundLabel(point_image)
