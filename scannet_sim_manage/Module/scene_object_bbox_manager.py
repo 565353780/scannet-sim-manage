0c485478fe841fc6679d707515cb5b959b3dbb07
chLi <565353780@qq.com>
2022-11-17 17:01:56 +0800

    add basic bbox manager

---
+++ scannet_sim_manage/Module/scene_object_bbox_manager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scannet_sim_manage.Data.bbox_group import BBoxGroup
+
+class SceneObjectBBoxManager(object):
+    def __init__(self):
+        self.bbox_group_dict = {}
+        return
+
+    def reset(self):
+        self.bbox_group_dict = {}
+        return True
+
+    def addBBox(self, bbox):
+        return True
