89fed1056c3cdd980efa9d58db881c716bc78fd7
chLi <565353780@qq.com>
2022-11-26 20:58:34 +0800

    add points in bbox check

--- scannet_sim_manage/Method/render.py
+++ scannet_sim_manage/Method/render.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from multiprocessing import Process
+
 import numpy as np
 import open3d as o3d
-from multiprocessing import Process
 
 from scannet_sim_manage.Method.bbox import getOpen3DBBoxFromBBox
 
@@ -39,6 +40,27 @@ def renderPointImage(point_image):
     assert render([pcd])
     return True
 
+def isAnyPointInBBox(bbox, point_array):
+    if len(point_array) == 0:
+        return False
+
+    mask_x = (point_array[:, 0] >= bbox.min_point.x) &\
+        (point_array[:, 0] <= bbox.max_point.x)
+    if True not in mask_x:
+        return False
+
+    mask_y = (point_array[:, 1] >= bbox.min_point.y) &\
+        (point_array[:, 1] <= bbox.max_point.y)
+    if True not in mask_y:
+        return False
+
+    mask_z = (point_array[:, 2] >= bbox.min_point.z) &\
+        (point_array[:, 2] <= bbox.max_point.z)
+    if True not in mask_z:
+        return False
+
+    mask = mask_x & mask_y & mask_z
+    return True in mask
 
 def renderAll(point_image, bbox_dict):
     points = point_image.point_array[np.where(
@@ -54,6 +76,8 @@ def renderAll(point_image, bbox_dict):
 
     bbox_list = []
     for bbox in bbox_dict.values():
+        if not isAnyPointInBBox(bbox, points):
+            continue
         bbox_list.append(getOpen3DBBoxFromBBox(bbox))
 
     assert render([pcd] + bbox_list)
