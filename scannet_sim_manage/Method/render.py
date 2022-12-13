0b9e1e6a92deb56507ac8285f0ecdcdc957b11f8
chLi <565353780@qq.com>
2022-12-13 14:57:16 +0800

    finish rendering labels

--- scannet_sim_manage/Method/render.py
+++ scannet_sim_manage/Method/render.py
@@ -41,17 +41,30 @@ def getLabeledPointImagePCD(point_image, label_color_dict={}):
     point_idx = np.where(point_image.point_array[:, 0] != float("inf"))[0]
 
     points = point_image.point_array[point_idx]
+    colors = []
+
+    background_color = np.zeros(3)
 
     for i in point_idx:
         label_dict = point_image.label_dict_list[i]
-        print(label_dict)
-        if i > 100:
-            break
-    exit()
+        if "background" in label_dict.keys():
+            colors.append(background_color)
+            continue
 
-    colors = point_image.image.reshape(-1, 3)[np.where(
-        point_image.point_array[:, 0] != float("inf"))[0]][...,
-                                                           [2, 1, 0]] / 255.0
+        assert "empty" not in label_dict.keys()
+
+        for key in label_dict.keys():
+            if key in label_color_dict.keys():
+                continue
+
+            new_color = np.random.rand(3) * 255.0
+            label_color_dict[key] = new_color
+
+        label = list(label_dict.keys())[0]
+        color = label_color_dict[label]
+        colors.append(color)
+
+    colors = np.array(colors, dtype=float) / 255.0
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
@@ -83,6 +96,9 @@ def isAnyPointInBBox(bbox, point_array):
 
 
 def getValidBBoxPCDList(bbox_dict, point_image):
+    points = point_image.point_array[np.where(
+        point_image.point_array[:, 0] != float("inf"))[0]]
+
     bbox_list = []
     for bbox in bbox_dict.values():
         if not isAnyPointInBBox(bbox, points):
