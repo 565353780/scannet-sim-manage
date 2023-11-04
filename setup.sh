b72c0e969e5641c9eeaf5691de7c23019ade7583
chLi <565353780@qq.com>
2023-11-04 19:05:06 +0800

    fix install error

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ cd ..
 git clone https://github.com/565353780/mesh-manage.git
 git clone https://github.com/565353780/habitat-sim-manage.git
 git clone https://github.com/565353780/detectron2-detect.git
+git clone https://github.com/565353780/scene-layout-detect.git
 
 cd mesh-manage
 ./setup.sh
@@ -10,4 +11,7 @@ cd ../habitat-sim-manage
 ./setup.sh
 
 cd ../detectron2-detect
+./setup.sh
+
+cd ../scene-layout-detect
 ./setup.sh
